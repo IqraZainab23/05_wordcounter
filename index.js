cbfeb27ac9d2858bcee1923a7e6a35857455a299
IqraZainab23 <iqrazainab19@gmail.com>
2024-02-01 00:51:54 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bgBlueBright("Word Counter"));
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: (chalk.green("Enter your sentence to count the word: "))
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+console.log(chalk.cyan(`Your sentence word count is ${words.length}`));
